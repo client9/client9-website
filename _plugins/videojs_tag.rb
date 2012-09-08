c4df9b2af5bf7e4db1cbb29efd8d0a4c0bf50d2b
Nick Galbreath <nickg@client9.com>
2012-09-08 09:50:29 -0400

    fix video sizing

--- _plugins/videojs_tag.rb
+++ _plugins/videojs_tag.rb
@@ -2,7 +2,7 @@ module Jekyll
   module VideoJS
     def videojs(code)
       "\n<video id='my_video_1' controls='1' class='video-js vjs-default-skin'" +
-      " preload='auto' width='640' height='264' poster=''" +
+      " preload='auto' width='600' height='300' poster=''" +
       " data-setup='{}' >\n" +
       "  <source src='#{code}' type='video/mp4' />\n" +
       "</video>\n"
